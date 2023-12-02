6ea5944a24314261ab529e00f2488811951c96ea
mnshimiyimana <m.nshimiyim@alustudent.com>
2023-12-02 13:33:55 +0200

    Modified

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,9 +4,9 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-    <h1>Hello</h1>
+    {/* <h1>Hello</h1>
     <Link className='text-blue-400' href="/about"> here
-    </Link>
+    </Link> */}
     </div>
   )
 }
